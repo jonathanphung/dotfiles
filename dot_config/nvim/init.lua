1ef492ff8572cf5412ad6ba0f3c51416f84bf436
Jonathan Phung <133593720+jonathanphung@users.noreply.github.com>
2026-07-21 05:23:30 -0500

    Update .config/nvim/init.lua Update .config/starship.toml

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -171,6 +171,9 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- Shell used by :terminal
+  vim.o.shell = 'zsh'
 end
 
 -- ============================================================
