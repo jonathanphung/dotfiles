eca6470d08ff519a287c5ffb84d5822eb49bb65b
Jonathan Phung <133593720+jonathanphung@users.noreply.github.com>
2026-07-21 02:44:09 -0500

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,6 +4,7 @@ fish_add_path ~/.local/bin
 
 abbr -a n nvim
 abbr -a leet 'cd ~/repos/leetcode && nvim .'
+abbr -a dev 'kitty @ launch --location=vsplit --cwd=current lazygit && exec claude'
 
 fish_vi_key_bindings
 
